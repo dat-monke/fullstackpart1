fc4db2d5decb6ea4e01940402336ed9303df7af2
David Taing <dtaing1@uci.edu>
2023-02-24 14:37:32 -0800

    header checkpoint

--- courseinfo/src/App.js
+++ courseinfo/src/App.js
@@ -7,30 +7,15 @@ const Header = (props) => {
   )
 }
 
-const Content = (props) => {
-  console.log(props)
-  return (
-    <div>
-      <p>
-        {props.part} {props.exercises}
-      </p>
-    </div>
-  )
-}
-
 const App = () => {
   const course = 'Half Stack application development'
-  const courseInfo = [
-    {part:'Fundamentals of React', exercise: 10},
-    {part:'Using props to pass data', exercise: 7},
-    {part:'State of a component', exercise: 14},
-  ]
 
   return (
-    <div>
+    <>
       <Header course={course} />
-      <Content part={courseInfo[0].part} exercises={courseInfo[0].exercise} />
-    </div>
+      {/* <Content/>
+      <Footer/> */}
+    </>
   )
 }
 
