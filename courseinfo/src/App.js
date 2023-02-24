28e886dafeb90bca783182c64697b4b60136f25c
David Taing <dtaing1@uci.edu>
2023-02-24 14:30:03 -0800

    header complete

--- courseinfo/src/App.js
+++ courseinfo/src/App.js
@@ -1,25 +1,35 @@
+const Header = (props) => {
+  console.log(props)
+  return (
+    <div> 
+      <h1>{props.course}</h1>
+    </div>
+  )
+}
+
+const Content = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <p>
+        {props.part} {props.exercises}
+      </p>
+    </div>
+  )
+}
+
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const courseInfo = [
+    {part:'Fundamentals of React', exercise: 10},
+    {part:'Using props to pass data', exercise: 7},
+    {part:'State of a component', exercise: 14},
+  ]
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course={course} />
+      <Content part={courseInfo[0].part} exercises={courseInfo[0].exercise} />
     </div>
   )
 }
