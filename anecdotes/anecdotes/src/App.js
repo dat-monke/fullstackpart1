1c5c7c910459a974e964aec6a04b311f24e9e11b
David Taing <dtaing1@uci.edu>
2023-03-07 13:36:37 -0800

    anecdotes randomly selected

--- anecdotes/anecdotes/src/App.js
+++ anecdotes/anecdotes/src/App.js
@@ -1,5 +1,8 @@
 import { useState } from 'react'
 
+const Button = (props) => <button onClick={props.handleClick}>{props.text}</button>
+
+
 const App = () => {
   const anecdotes = [
     'If it hurts, do it more often.',
@@ -16,7 +19,8 @@ const App = () => {
 
   return (
     <div>
-      {anecdotes[selected]}
+      <p>{anecdotes[selected]}</p>
+      <Button handleClick={() => setSelected(selected + 1)} text="next anecdote"/>
     </div>
   )
 }
